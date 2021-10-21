89c1f0c8e0059976a93343caa88d6e32f13320b5
Dustin Flynn <flynnd297@gmail.com>
2021-10-20 19:37:02 -0500

    Added save button functionality by taking the data in the text fields and assigning it to the table grid.

--- NoteTakingForm/NoteTakingForm/Form1.Designer.cs
+++ NoteTakingForm/NoteTakingForm/Form1.Designer.cs
@@ -30,6 +30,15 @@ namespace NoteTakingForm
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bttnNew = new System.Windows.Forms.Button();
+            this.bttnSave = new System.Windows.Forms.Button();
+            this.bttnRead = new System.Windows.Forms.Button();
+            this.bttnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -37,18 +46,101 @@ namespace NoteTakingForm
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(124, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(183, 83);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 2;
+            this.txtTitle.Text = "txtTitle";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(183, 122);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(200, 129);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.Text = "txtMessage";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(443, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(177, 193);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // bttnNew
+            // 
+            this.bttnNew.Location = new System.Drawing.Point(183, 359);
+            this.bttnNew.Name = "bttnNew";
+            this.bttnNew.Size = new System.Drawing.Size(75, 23);
+            this.bttnNew.TabIndex = 5;
+            this.bttnNew.Text = "New";
+            this.bttnNew.UseVisualStyleBackColor = true;
+            this.bttnNew.Click += new System.EventHandler(this.bttnNew_Click);
+            // 
+            // bttnSave
+            // 
+            this.bttnSave.Location = new System.Drawing.Point(308, 359);
+            this.bttnSave.Name = "bttnSave";
+            this.bttnSave.Size = new System.Drawing.Size(75, 23);
+            this.bttnSave.TabIndex = 6;
+            this.bttnSave.Text = "Save";
+            this.bttnSave.UseVisualStyleBackColor = true;
+            this.bttnSave.Click += new System.EventHandler(this.bttnSave_Click);
+            // 
+            // bttnRead
+            // 
+            this.bttnRead.Location = new System.Drawing.Point(443, 359);
+            this.bttnRead.Name = "bttnRead";
+            this.bttnRead.Size = new System.Drawing.Size(75, 23);
+            this.bttnRead.TabIndex = 7;
+            this.bttnRead.Text = "Read";
+            this.bttnRead.UseVisualStyleBackColor = true;
+            this.bttnRead.Click += new System.EventHandler(this.bttnRead_Click);
+            // 
+            // bttnDelete
+            // 
+            this.bttnDelete.Location = new System.Drawing.Point(545, 359);
+            this.bttnDelete.Name = "bttnDelete";
+            this.bttnDelete.Size = new System.Drawing.Size(75, 23);
+            this.bttnDelete.TabIndex = 8;
+            this.bttnDelete.Text = "Delete";
+            this.bttnDelete.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttnDelete);
+            this.Controls.Add(this.bttnRead);
+            this.Controls.Add(this.bttnSave);
+            this.Controls.Add(this.bttnNew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +149,14 @@ namespace NoteTakingForm
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bttnNew;
+        private System.Windows.Forms.Button bttnSave;
+        private System.Windows.Forms.Button bttnRead;
+        private System.Windows.Forms.Button bttnDelete;
     }
 }
 
